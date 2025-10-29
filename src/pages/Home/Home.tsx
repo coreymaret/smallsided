d3219f438d283d695ffdabccdcc01c625dc0d1e7
coreymaret <coreymaret@gmail.com>
2025-10-29 18:17:16 -0400

    Added a carousel. It needs work.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,21 +1,21 @@
-import styles from "./Home.module.scss";
+import Carousel from '../../components/Carousel/Carousel';
 
-
-const Home = () => (
-  <>
-    <h2 className={styles.title}>Home</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    <h3>Home Link 1</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.Lorem ipsum dolor sit amet, consectetur adipiscing elite.</p>
-  </>
-);
+const Home = () => {
+  return (
+    <div className="home">
+      <section className="hero">
+        {/* Your hero section content */}
+      </section>
+      
+      <section className="carousel-section">
+        <Carousel />
+      </section>
+      
+      <section className="other-content">
+        {/* Other home page content */}
+      </section>
+    </div>
+  );
+};
 
 export default Home;
