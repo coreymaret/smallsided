b2a41cf5c77c260a1b1a351d232fee48b8a92be6
coreymaret <coreymaret@gmail.com>
2025-11-07 12:57:47 -0500

    Editing popup

--- src/components/Popup/Popup.tsx
+++ src/components/Popup/Popup.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./Popup.module.scss";
 import Logo from "../../assets/Logo.svg";
 
