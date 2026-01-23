571ff6e5e1f20c6aa950c79ed7815f6150dd46f4
coreymaret <coreymaret@gmail.com>
2026-01-23 07:13:10 -0500

    Fixed RegisterLeague

--- src/components/RegisterLeague/RegisterLeague.tsx
+++ src/components/RegisterLeague/RegisterLeague.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Users, Calendar, ChevronRight, Check, Trophy, Mail, Phone, User, Shield, CreditCard, Lock } from 'lucide-react';
+import { Users, Calendar, ChevronRight, Check, Trophy, Mail, User, Shield, CreditCard, Lock } from 'lucide-react';
 import styles from './RegisterLeague.module.scss';
 
 const RegisterLeague: React.FC = () => {
