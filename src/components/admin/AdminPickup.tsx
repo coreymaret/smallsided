5d21ba8027a6a4002c8dce766668e77ecbef3f70
coreymaret <coreymaret@gmail.com>
2026-01-28 12:23:47 -0500

    Changed AdminTable.module.scss to AdminTraining need to look into this

--- src/components/admin/AdminPickup.tsx
+++ src/components/admin/AdminPickup.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { supabase } from '../../lib/supabase';
 import { Search, Calendar, MapPin, Users } from 'lucide-react';
-import styles from './AdminTraiing.module.scss';
+import styles from './AdminTraining.module.scss';
 
 const AdminPickup = () => {
   const [bookings, setBookings] = useState<any[]>([]);
